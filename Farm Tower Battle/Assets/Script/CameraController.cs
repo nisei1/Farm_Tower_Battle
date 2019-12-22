5b29aef3a306545eea720cac4354baa42880672a
nisei1 <43508107+nisei1@users.noreply.github.com>
2019-12-23 00:44:47 +0900

    Change script
    http://blog.livedoor.jp/asamasou/archives/cat_149250.html

---
+++ Farm Tower Battle/Assets/Script/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public static bool isCollision;//衝突検出変数
+
+    /// <summary>
+    /// 衝突中を伝えるメソッド
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerStay(Collider collision)
+    {
+        isCollision = true;
+        //Debug.Log(isCollision);
+    }
+
+    /// <summary>
+    /// 衝突終了を伝えるメソッド
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerExit(Collider collision)
+    {
+        isCollision = false;
+    }
+}
